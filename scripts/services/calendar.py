16ace630f670ea63b341fac9bb007f4346483b26
TAgKyaw <dannynaing727@gmail.com>
2025-12-15 11:46:50 +0000

    updated calendar event method

--- scripts/services/calendar.py
+++ scripts/services/calendar.py
@@ -1,6 +1,17 @@
+from datetime import datetime
+#TODO: this is a mock calendar event, will get data from api after
 def get_todays_events():
-    # TODO: replace with Google Calendar API
-    # For now: mock event
     return [
-        {"title": "Meet friend", "time": "16:00", "location": "Central London"}
+        {
+            "title": "Meet Alex",
+            "start_time": "2025-12-15T16:00:00",
+            "location": "Central London",
+            "is_outside": True
+        },
+        {
+            "title": "Gym",
+            "start_time": "2025-12-15T19:00:00",
+            "location": "Local gym",
+            "is_outside": True
+        }
     ]
